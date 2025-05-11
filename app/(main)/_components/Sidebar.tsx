fae8426906c968b769bdff0ae3deb7b6a057f697
ebukachuqz <ebukachuqz@gmail.com>
2025-05-11 21:23:52 +0100

    feat: add Sidebar component with navigation links and logo

---
+++ app/(main)/_components/Sidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.png"
+            width={50}
+            height={50}
+            alt="MonieTeller logo"
+            // className="w-[40px] "
+          />
+          {/* <h1 className="sidebar-logo">MonieTeller</h1> */}
+          <div className="monieteller-logo">
+            <span className="monie">Monie</span>
+            <span className="teller">Teller</span>
+          </div>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,13 @@
+import Sidebar from "./_components/Sidebar";
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      SideBar
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar />
       {children}
     </main>
   );
