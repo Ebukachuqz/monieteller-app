3f116274402a99745efe64d0bf6e41c3da641fdd
ebukachuqz <ebukachuqz@gmail.com>
2025-05-20 19:24:38 +0100

    feat: Added banner to auth pages Layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <div>
+          <Image
+            src="/icons/moniteller-authbanner.png"
+            alt="Auth image"
+            width={600}
+            height={600}
+            className="rounded-l-xl object-cover border-4 border-blue-400"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
