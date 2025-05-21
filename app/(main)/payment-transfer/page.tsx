e1ede592ca8453f6f5fec8ae0decbf01a8cea30d
ebukachuqz <ebukachuqz@gmail.com>
2025-05-21 17:12:30 +0100

    feat: implement Payment Transfer page

--- app/(main)/payment-transfer/page.tsx
+++ app/(main)/payment-transfer/page.tsx
@@ -1,7 +1,15 @@
+import HeaderBox from "@/components/shared/HeaderBox";
 import React from "react";
 
 const PaymentTransferPage = () => {
-  return <div>PaymentTransferPage</div>;
+  return (
+    <section className="payment-transfer">
+      <HeaderBox
+        title="Payment Transfer"
+        subtext="Transfer money from your account"
+      />
+    </section>
+  );
 };
 
 export default PaymentTransferPage;
