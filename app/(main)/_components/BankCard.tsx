e74afe4bf357c682634353d19756a0d0d33a979f
ebukachuqz <ebukachuqz@gmail.com>
2025-05-21 17:09:00 +0100

    feat: implemented My banks page to render bank cards for each bank account

--- app/(main)/_components/BankCard.tsx
+++ app/(main)/_components/BankCard.tsx
@@ -13,7 +13,9 @@ const BankCard = ({
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{account.name}</h1>
-            <p className="font-ibm-plex-serif font-black text-white">12345</p>
+            <p className="font-ibm-plex-serif font-black text-white">
+              {account.balance}
+            </p>
           </div>
 
           <article className="flex flex-col gap-2">
@@ -22,7 +24,7 @@ const BankCard = ({
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">4321</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account.bvn}</span>
             </p>
           </article>
         </div>
--- app/(main)/my-banks/page.tsx
+++ app/(main)/my-banks/page.tsx
@@ -1,7 +1,39 @@
+import { getUserBanksDataAction, getUserDataAction } from "@/actions";
+import HeaderBox from "@/components/shared/HeaderBox";
 import React from "react";
+import BankCard from "../_components/BankCard";
 
-const MyBanksPage = () => {
-  return <div>MyBanksPage</div>;
+const MyBanksPage = async () => {
+  const userDocument: any = await getUserDataAction();
+  const userData = userDocument.documents[0];
+
+  const banksDocument: any = await getUserBanksDataAction();
+  const bankAccounts: any[] = banksDocument.documents;
+  return (
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox
+          title="My Banks"
+          subtext="Manage your bank accounts and transactions"
+        />
+
+        <div className="space-y-4">
+          <h2 className="header-2">Your Cards</h2>
+          <div className="flex flex-wrap gap-6">
+            {bankAccounts?.length > 0 &&
+              bankAccounts.map((account) => (
+                <BankCard
+                  key={account?.$id}
+                  account={account}
+                  userName={`${account.accountName}`}
+                  showBalance={false}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MyBanksPage;
